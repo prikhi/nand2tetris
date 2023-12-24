f535a1f2ba9696713994d84ae15f088e9014fae5
Pavan Rikhi <pavan.rikhi@gmail.com>
2023-12-23 23:50:54 -0500

    Project 10 - Finish Tokenizer
    Compiler now takes file or directory & tokenizes multiple inputs,
    writing the XML out to the appropriate file. It has been tested on each
    example project, unaltering the expected output files when whitespace is
    ignored.

--- Compiler.hs
+++ Compiler.hs
@@ -4,10 +4,21 @@ module Compiler (main) where
 
 import Compiler.Lexer (tok, tokenize)
 import Compiler.TokenRenderer (tokensToXml)
+import Control.Arrow ((&&&))
+import Control.Monad (forM_)
 import Data.List (intercalate)
-import Data.Text.IO qualified as T (putStr, readFile)
+import Data.Text.IO qualified as T (readFile, writeFile)
+import System.Directory (doesFileExist, getCurrentDirectory, listDirectory)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.FilePath
+    ( dropExtension
+    , splitExtension
+    , takeBaseName
+    , takeExtension
+    , (<.>)
+    , (</>)
+    )
 
 
 main :: IO ()
@@ -15,24 +26,46 @@ main =
     getArgs >>= \case
         [jackFileOrDir] ->
             runCompiler jackFileOrDir
+        [] ->
+            getCurrentDirectory >>= runCompiler
         args -> do
             putStrLn $
                 unlines
-                    [ "ERROR: Expected 1 argument, got: "
+                    [ "ERROR: Expected 0 or 1 arguments, got: "
                     , ""
                     , "\t" <> intercalate ", " (map show args)
                     , ""
-                    , "Compiler.hs <FILE_NAME>.jack"
+                    , "Compiler.hs [<FILE_NAME>.jack|<DIRECTORY>]"
                     , ""
-                    , "Reads input file into token stream."
-                    , "Prints out result or error."
+                    , "Reads input file or `.jack` files in directory."
+                    , "Tokenizes each file exiting with error on failure."
+                    , "Writes result to `<FILE_NAME>T.xml` in same directory as input file."
                     ]
             exitFailure
 
 
 runCompiler :: FilePath -> IO ()
 runCompiler jackPath = do
-    txt <- T.readFile jackPath
-    case tokenize jackPath txt of
-        Left e -> print e >> exitFailure
-        Right ts -> T.putStr $ tokensToXml $ map tok ts
+    files <- getFiles
+    forM_ files $ \(outputPath, filePath) -> do
+        txt <- T.readFile filePath
+        xml <- case tokenize filePath txt of
+            Left e -> print e >> exitFailure
+            Right ts -> return . tokensToXml $ map tok ts
+        T.writeFile outputPath xml
+  where
+    -- Get files to process & make the output file path
+    getFiles :: IO [(FilePath, FilePath)]
+    getFiles = do
+        isFile <- doesFileExist jackPath
+        if isFile
+            then case splitExtension jackPath of
+                (basePath, ".jack") ->
+                    return [(basePath <> "T" <.> "xml", jackPath)]
+                _ ->
+                    error "ERROR: Expected input file with extension '.vm"
+            else do
+                allFiles <- map (jackPath </>) <$> listDirectory jackPath
+                let jackFiles = filter ((== ".jack") . takeExtension) allFiles
+                    mkOutputPath fileName = jackPath </> dropExtension (takeBaseName fileName) <> "T" <.> "xml"
+                return $ map (mkOutputPath &&& id) jackFiles
